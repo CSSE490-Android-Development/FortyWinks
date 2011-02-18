7bd1250163c5dc466027c585aeecb2b11944cadd
Eric Stokes <fernferret@gmail.com>
2011-02-17 23:59:05 -0500

    Add empty Android project

---
+++ Android-Application/src/com/fernferret/android/fortywinks/FortyWinks.java
@@ -0,0 +1,13 @@
+package com.fernferret.android.fortywinks;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FortyWinks extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
