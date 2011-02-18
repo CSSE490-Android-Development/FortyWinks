2300ab5900785452cf978366ee5cb0699e7c92fe
Jimmy Theis <jimmy@jetheis.com>
2011-02-18 01:54:30 -0500

    Add test cases for setting and unsetting days
    Signed-off-by: Jimmy Theis <jimmy@jetheis.com>

--- Android-Test/src/com/fernferret/android/fortywinks/test/AlarmTests.java
+++ Android-Test/src/com/fernferret/android/fortywinks/test/AlarmTests.java
@@ -10,4 +10,161 @@ public class AlarmTests extends TestCase {
         Alarm a = new Alarm(1);
         assertNotNull("Alarm was NULL", a);
     }
+    
+    public void testAlarmSetsInitialDaysOfWeek() {
+        Alarm a = new Alarm(1);
+        assertEquals(a.getDaysOfWeek(), 0);
+    }
+    
+    public void testAlarmSetDay() {
+        Alarm a = new Alarm(1);
+        a.enableDay(Alarm.Day.SUNDAY);
+        assertTrue(a.isDayEnabled(Alarm.Day.SUNDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.MONDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.TUESDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.WEDNESDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.THURSDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.FRIDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.SATURDAY));
+        
+        a = new Alarm(1);
+        a.enableDay(Alarm.Day.MONDAY);
+        assertTrue(a.isDayEnabled(Alarm.Day.MONDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.SUNDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.TUESDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.WEDNESDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.THURSDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.FRIDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.SATURDAY));
+        
+        a = new Alarm(1);
+        a.enableDay(Alarm.Day.TUESDAY);
+        assertTrue(a.isDayEnabled(Alarm.Day.TUESDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.MONDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.SUNDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.WEDNESDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.THURSDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.FRIDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.SATURDAY));
+        
+        a = new Alarm(1);
+        a.enableDay(Alarm.Day.WEDNESDAY);
+        assertTrue(a.isDayEnabled(Alarm.Day.WEDNESDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.MONDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.TUESDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.SUNDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.THURSDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.FRIDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.SATURDAY));
+        
+        a = new Alarm(1);
+        a.enableDay(Alarm.Day.THURSDAY);
+        assertTrue(a.isDayEnabled(Alarm.Day.THURSDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.MONDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.TUESDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.WEDNESDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.SUNDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.FRIDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.SATURDAY));
+        
+        a = new Alarm(1);
+        a.enableDay(Alarm.Day.FRIDAY);
+        assertTrue(a.isDayEnabled(Alarm.Day.FRIDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.MONDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.TUESDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.WEDNESDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.THURSDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.SUNDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.SATURDAY));
+        
+        a = new Alarm(1);
+        a.enableDay(Alarm.Day.SATURDAY);
+        assertTrue(a.isDayEnabled(Alarm.Day.SATURDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.MONDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.TUESDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.WEDNESDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.THURSDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.FRIDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.SUNDAY));
+    }
+    
+    public void testAlarmUnsetDay() {
+        Alarm a = new Alarm(1);
+        a.enableDay(Alarm.Day.SUNDAY);
+        a.disableDay(Alarm.Day.SUNDAY);
+        assertFalse(a.isDayEnabled(Alarm.Day.SUNDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.MONDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.TUESDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.WEDNESDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.THURSDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.FRIDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.SATURDAY));
+        
+        a = new Alarm(1);
+        a.enableDay(Alarm.Day.MONDAY);
+        a.disableDay(Alarm.Day.MONDAY);
+        assertFalse(a.isDayEnabled(Alarm.Day.SUNDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.MONDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.TUESDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.WEDNESDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.THURSDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.FRIDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.SATURDAY));
+        
+        a = new Alarm(1);
+        a.enableDay(Alarm.Day.TUESDAY);
+        a.disableDay(Alarm.Day.TUESDAY);
+        assertFalse(a.isDayEnabled(Alarm.Day.SUNDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.MONDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.TUESDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.WEDNESDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.THURSDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.FRIDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.SATURDAY));
+        
+        a = new Alarm(1);
+        a.enableDay(Alarm.Day.WEDNESDAY);
+        a.disableDay(Alarm.Day.WEDNESDAY);
+        assertFalse(a.isDayEnabled(Alarm.Day.SUNDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.MONDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.TUESDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.WEDNESDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.THURSDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.FRIDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.SATURDAY));
+        
+        a = new Alarm(1);
+        a.enableDay(Alarm.Day.THURSDAY);
+        a.disableDay(Alarm.Day.THURSDAY);
+        assertFalse(a.isDayEnabled(Alarm.Day.SUNDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.MONDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.TUESDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.WEDNESDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.THURSDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.FRIDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.SATURDAY));
+        
+        a = new Alarm(1);
+        a.enableDay(Alarm.Day.FRIDAY);
+        a.disableDay(Alarm.Day.FRIDAY);
+        assertFalse(a.isDayEnabled(Alarm.Day.SUNDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.MONDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.TUESDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.WEDNESDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.THURSDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.FRIDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.SATURDAY));
+        
+        a = new Alarm(1);
+        a.enableDay(Alarm.Day.SATURDAY);
+        a.disableDay(Alarm.Day.SATURDAY);
+        assertFalse(a.isDayEnabled(Alarm.Day.SUNDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.MONDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.TUESDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.WEDNESDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.THURSDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.FRIDAY));
+        assertFalse(a.isDayEnabled(Alarm.Day.SATURDAY));
+    }
+    
 }
