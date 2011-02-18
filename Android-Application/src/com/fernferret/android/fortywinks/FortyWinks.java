77a38aaf6888ddb8ae8b03d27c8af74db682fb9a
Eric Stokes <fernferret@gmail.com>
2011-02-18 02:55:04 -0500

    Add code stubs for Main class

--- Android-Application/src/com/fernferret/android/fortywinks/FortyWinks.java
+++ Android-Application/src/com/fernferret/android/fortywinks/FortyWinks.java
@@ -2,11 +2,29 @@ package com.fernferret.android.fortywinks;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class FortyWinks extends Activity {
+	
+	// Load UI Resources
+	// TextViews
+	TextView mBigTime;
+	TextView mTimeLeftToSleep;
+	TextView mRemainingCycles;
+	
+	// Buttons
+	Button mDoSleepButton;
+	
+	// ListViews
+	ListView mQuickAlarmList;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        
     }
 }
