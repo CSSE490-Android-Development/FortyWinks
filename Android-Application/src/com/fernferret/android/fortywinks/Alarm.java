101629cc7d72681820326918ccf8c09e4139489f
Jimmy Theis <jimmy@jetheis.com>
2011-02-18 00:54:14 -0500

    Add initial class files
    Signed-off-by: Jimmy Theis <jimmy@jetheis.com>

---
+++ Android-Application/src/com/fernferret/android/fortywinks/Alarm.java
@@ -0,0 +1,19 @@
+package com.fernferret.android.fortywinks;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Alarm implements Parcelable {
+
+    @Override
+    public int describeContents() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        // TODO Auto-generated method stubs
+    }
+
+}
---
+++ Android-Application/src/com/fernferret/android/fortywinks/AlarmLineItem.java
@@ -0,0 +1,13 @@
+package com.fernferret.android.fortywinks;
+
+import android.content.Context;
+import android.widget.RelativeLayout;
+
+public class AlarmLineItem extends RelativeLayout {
+
+    public AlarmLineItem(Context context) {
+        super(context);
+        // TODO Auto-generated constructor stub
+    }
+
+}
