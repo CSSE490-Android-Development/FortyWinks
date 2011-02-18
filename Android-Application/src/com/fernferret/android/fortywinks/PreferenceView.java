bd62e8befec5f8e2a4395eece9db87329d2cbf04
Eric Stokes <fernferret@gmail.com>
2011-02-18 02:45:52 -0500

    Add Code for PrefView

--- Android-Application/src/com/fernferret/android/fortywinks/PreferenceView.java
+++ Android-Application/src/com/fernferret/android/fortywinks/PreferenceView.java
@@ -1,13 +1,55 @@
 package com.fernferret.android.fortywinks;
 
+import android.app.Activity;
 import android.content.Context;
-import android.widget.RelativeLayout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
-public class PreferenceView extends RelativeLayout {
-
-    public PreferenceView(Context context) {
-        super(context);
-        // TODO Auto-generated constructor stub
-    }
-
+public class PreferenceView extends TableLayout {
+	
+	private Activity mContext;
+	private TextView mLeftTextView;
+	private TextView mRightTextView;
+	private TableRow mTableRow;
+	
+	public PreferenceView(Context context) {
+		super(context);
+		mContext = (Activity) context;
+		
+		LayoutInflater layoutInflater = mContext.getLayoutInflater();
+		layoutInflater.inflate(R.layout.preference_view, this);
+		
+		mTableRow = (TableRow) findViewById(R.id.preference_view_row);
+		mLeftTextView = (TextView) findViewById(R.id.preference_view_left_text);
+		mRightTextView = (TextView) findViewById(R.id.preference_view_right_text);
+	}
+	
+	private View.OnClickListener mOnRowClickListener = new View.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			
+		}
+	};
+	
+	public void setLeftText(String text) {
+		mLeftTextView.setText(text);
+	}
+	
+	public void setLeftAlign(int g) {
+		mRightTextView.setGravity(g);
+	}
+	
+	public void setRightText(String text) {
+		mRightTextView.setText(text);
+	}
+	
+	public void setRightAlign(int g) {
+		mRightTextView.setGravity(g);
+	}
+	
+	
+	
 }
