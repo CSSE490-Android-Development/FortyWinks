e512f2ae12003d5bb7615eb733a338a221d2f716
Jimmy Theis <jimmy@jetheis.com>
2011-02-18 01:09:56 -0500

    Add getters, setters, constructor to Alarm
    Signed-off-by: Jimmy Theis <jimmy@jetheis.com>

--- Android-Application/src/com/fernferret/android/fortywinks/Alarm.java
+++ Android-Application/src/com/fernferret/android/fortywinks/Alarm.java
@@ -4,6 +4,34 @@ import android.os.Parcel;
 import android.os.Parcelable;
 
 public class Alarm implements Parcelable {
+    
+    private int mId;
+    private int mFollowups;
+    private int mIntervalStart;
+    private int mIntervalEnd;
+    private int mDaysOfWeek;
+    private boolean mEnabled;
+    
+    public Alarm(int id) {
+        mId = id;
+    }
+    
+    public int getId() { return mId; }
+    
+    public int getFollowups() { return mFollowups; }
+    public void setFollowups(int followups) { mFollowups = followups; }
+    
+    public int getIntervalStart() { return mIntervalStart; }
+    public void setIntervalStart(int intervalStart) { mIntervalStart = intervalStart; }
+    
+    public int getIntervalEnd() { return mIntervalEnd; }
+    public void setIntervalEnd(int intervalEnd) { mIntervalEnd = intervalEnd; }
+    
+    public int getDaysOfWeek() { return mDaysOfWeek; }
+    public void setDaysOfWeek(int daysOfWeek) { mDaysOfWeek = daysOfWeek; }
+    
+    public boolean getEnabled() { return mEnabled; }
+    public void setEnabled(boolean enabled) { mEnabled = enabled; }
 
     @Override
     public int describeContents() {
