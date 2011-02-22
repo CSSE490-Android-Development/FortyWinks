458c89afeed1634818fcc2e1e5cdd551c237dca7
Jimmy Theis <jimmy@jetheis.com>
2011-02-21 20:59:15 -0500

    Fix an import
    Signed-off-by: Jimmy Theis <jimmy@jetheis.com>

--- Android-Application/src/com/fernferret/android/fortywinks/EditAlarm.java
+++ Android-Application/src/com/fernferret/android/fortywinks/EditAlarm.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
