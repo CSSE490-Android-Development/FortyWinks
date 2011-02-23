5681e6d13e0731601f6120cd700c6345386ba7b0
Jimmy Theis <jimmy@jetheis.com>
2011-02-23 10:43:52 -0500

    Fix imports
    Signed-off-by: Jimmy Theis <jimmy@jetheis.com>

--- Android-Application/src/com/fernferret/android/fortywinks/SQLiteAdapter.java
+++ Android-Application/src/com/fernferret/android/fortywinks/SQLiteAdapter.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import android.content.Context;
 import android.database.Cursor;
